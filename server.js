545b0c079f35602549f85ef9b9e7e6041eaabfb7
raworiginal <randall.a.wolfe@gmail.com>
2025-07-18 12:33:51 -0700

    Connected to MongoDB

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const mongoose = require("mongoose");
 /* ========================== Constants ========================== */
 const app = express();
 
+/* ====================== DB Connection ======================*/
+mongoose.connect(process.env.MONGODB_URI);
+//log conneciton status in terminal at start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
 /* ========================== Routes ========================== */
 app.get("/", async (req, res) => {
   res.render("index.ejs");
